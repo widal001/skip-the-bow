0c32a2b24a566a02fe0f858f3d904893ed5cf412
widal001 <williamdaly422@gmail.com>
2025-04-18 19:15:39 -0400

    build: Switches astro to hybrid rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import node from "@astrojs/node";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "static",
+  adapter: node({
+    mode: "standalone",
+  }),
+  experimental: {
+    session: true,
+  },
+});
