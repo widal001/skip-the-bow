0ea0864f662f42895df6b44c0ef733f59f0dcfde
widal001 <williamdaly422@gmail.com>
2025-04-20 17:16:45 -0400

    feat: Sets up cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import node from "@astrojs/node";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   output: "static",
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: cloudflare(),
   experimental: {
     session: true,
   },
