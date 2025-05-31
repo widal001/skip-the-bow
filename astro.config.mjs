3490c1ac9b7475cfc91e7481867932a69963bab3
widal001 <williamdaly422@gmail.com>
2025-05-30 20:17:54 -0400

    feat: Add auth-astro plugin for authentication

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,16 @@
 import { defineConfig } from "astro/config";
 
 import cloudflare from "@astrojs/cloudflare";
+import auth from "auth-astro";
 
 // https://astro.build/config
 export default defineConfig({
   output: "static",
   adapter: cloudflare(),
+
   experimental: {
     session: true,
   },
+
+  integrations: [auth()],
 });
