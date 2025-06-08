81091987313a20653dd287995f8a6c12d4464ac1
widal001 <williamdaly422@gmail.com>
2025-06-08 16:49:44 -0400

    refactor: Switch to netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import cloudflare from "@astrojs/cloudflare";
 import auth from "auth-astro";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   output: "static",
-  adapter: cloudflare(),
+  adapter: netlify(),
 
   experimental: {
     session: true,
