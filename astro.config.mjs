5531912c3740e94299cbdcbfbd2df455c284f067
widal001 <williamdaly422@gmail.com>
2025-06-14 15:08:01 -0400

    build: Switches to Netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import cloudflare from "@astrojs/cloudflare";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   output: "static",
-  adapter: cloudflare(),
+  adapter: netlify(),
   experimental: {
     session: true,
   },
